73f729575e656034ea457ed5c54496aef3703fa2
Mikael-Leger <mikael.leger8@gmail.com>
2024-12-10 16:37:11 +0100

    vercel/update-postbuild

--- frontend/scripts/postbuild.mjs
+++ frontend/scripts/postbuild.mjs
@@ -1,25 +1,27 @@
 import fs from 'fs-extra';
 import path from 'path';
 
-// Définir les chemins des dossiers
 const distPath = path.join(process.cwd(), 'dist');
-const templatesPath = path.resolve(process.cwd(), '..', 'backend', 'templates'); // Revenir d'un niveau avec '..'
-const staticPath = path.resolve(process.cwd(), '..', 'backend', 'static'); // Revenir d'un niveau avec '..'
+const templatesPath = path.resolve(process.cwd(), '..', 'backend', 'templates');
+const staticPath = path.resolve(process.cwd(), '..', 'backend', 'static');
 
-// Déplacer index.html vers templates
 const indexPath = path.join(distPath, 'index.html');
-console.log(indexPath); // Affiche le chemin d'index.html
 const targetIndexPath = path.join(templatesPath, 'index.html');
-console.log(targetIndexPath); // Affiche le chemin cible de index.html
 fs.moveSync(indexPath, targetIndexPath, { overwrite: true });
 
-// Déplacer le reste des fichiers vers static
+if (!fs.existsSync(staticPath)) {
+  fs.mkdirSync(staticPath, { recursive: true });
+}
+
 const files = fs.readdirSync(distPath);
+console.log('Fichiers dans dist:', files);
+
 files.forEach(file => {
   const src = path.join(distPath, file);
   const dest = path.join(staticPath, file);
+  
+  console.log(`Déplacement de ${src} vers ${dest}`);
   fs.moveSync(src, dest, { overwrite: true });
 });
 
-// Supprimer le dossier dist après le déplacement
 fs.removeSync(distPath);
